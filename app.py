cc8691e7649e398d28d92c3466695b7847774e84
yashwardhan-rao <raoyashwardhan31@gmail.com>
2026-05-14 21:20:29 +0530

    feat: establish basic front n backend communication

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/action", methods=["POST"])
+def action():
+    data = request.json
+    user_message = data.get("action", "")
+
+    return jsonify({"reply": f"Python received: {user_message}"})
 
 if __name__ == "__main__":
     app.run(debug=True)
